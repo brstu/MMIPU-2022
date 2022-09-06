0e413d62bb9cd87770811ce9914bc8b6c535627c
Pandem1r <Gor.NikitaS@mail.ru>
2022-09-06 15:39:46 +0300

    Без нелинейной модели

---
+++ trunk/as0005907/task_01/src/Lab1_MMIPU.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+const int a = 1, b = 1, c = 1, d = 1;
+void calculateLinearModel(int time, float yCurrent, const float& inputWarm);
+//float calculateNonlinearModel(); Work in progress
+int main()
+{
+	float yCurrent = 0.0, inputWarm = 0.0;
+	int time = 0;
+	do
+	{
+		std::cout << "Pls input time (positive): "; std::cin >> time;
+	} while (time <= 0);
+	std::cout << "Input a temperature: "; std::cin >> yCurrent;
+	std::cout << "Input a warm: "; std::cin >> inputWarm;
+	calculateLinearModel(time, yCurrent, inputWarm);
+	return 0;
+}
+void calculateLinearModel(int time, float yCurrent, const float& inputWarm)
+{
+	float yNext = 0.0;
+	while (time--)
+	{
+		yNext = (a * yCurrent) + (b * inputWarm);
+		std::cout << yNext << std::endl;
+		yCurrent = yNext;
+	}
+}
+
+//float calculateNonlinearModel()
+//{
+//	return;
+//}
