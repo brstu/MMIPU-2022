246e09a80bfa13ce11a6d047efbed63ca7e81ff2
dima203 <gg37500@yandex.by>
2022-09-28 13:00:26 +0300

    change code

--- trunk/as0005912/task_01/src/main.cpp
+++ trunk/as0005912/task_01/src/main.cpp
@@ -7,12 +7,13 @@
 class TemperatureCalculator
 {
 public:
-    virtual ~TemperatureCalculator(){};
     virtual double CalculateNextTemperature(double input_warm) = 0;
+protected:
+    TemperatureCalculator(){};
 };
 
 
-class LinearTemperatureCalculator: TemperatureCalculator 
+class LinearTemperatureCalculator: public TemperatureCalculator 
 {
 private:
     double _a, _b;
@@ -25,17 +26,15 @@ public:
         _current_temperature = current_temperature;
     }
 
-    double CalculateNextTemperature(double input_warm) 
+    virtual double CalculateNextTemperature(double input_warm) 
     {
         _current_temperature = _a * _current_temperature + _b * input_warm;
         return _current_temperature;
     }
-
-    ~LinearTemperatureCalculator(){}
 };
 
 
-class NonlinearTemperatureCalculator: TemperatureCalculator
+class NonlinearTemperatureCalculator: public TemperatureCalculator
 {
 private:
     double _a, _b, _c, _d;
@@ -51,15 +50,13 @@ public:
         _previous_temperature = 0.0;
     }
 
-    double CalculateNextTemperature(double input_warm) 
+    virtual double CalculateNextTemperature(double input_warm) 
     {
         double buffer_temperature = _current_temperature;
         _current_temperature = _a * _current_temperature - _b * _previous_temperature * _previous_temperature + _c * input_warm + _d * sin(input_warm);
         _previous_temperature = buffer_temperature;
         return _current_temperature;
     }
-
-    ~NonlinearTemperatureCalculator(){}
 };
 
 
@@ -95,47 +92,47 @@ int main()
     TemperatureViewer* temperature_viewer;
     double a, b, c, d, current_temperature;
     int strategy_choise;
-    std::cout << "Выберите способ расчета температуры (0 - линейный, 1 - нелинейный):" << std::endl;
+    std::cout << "Выберите метод рассчета температуры (0 - линейный, 1 - нелинейный):" << std::endl;
     std::cin >> strategy_choise;
 
     switch (strategy_choise) {
-    case 0:
-    {
-        std::cout << "A: ";
-        std::cin >> a;
-        std::cout << "B: ";
-        std::cin >> b;
-        std:: cout << "Текущая температура: ";
-        std::cin >> current_temperature;
-        LinearTemperatureCalculator* linear_temperature_calculator = new LinearTemperatureCalculator(current_temperature, a, b);
-        temperature_viewer = new TemperatureViewer(reinterpret_cast<TemperatureCalculator*>(linear_temperature_calculator));
-        break;
-    }
-    case 1:
-    {
-        std::cout << "A: ";
-        std::cin >> a;
-        std::cout << "B: ";
-        std::cin >> b;
-        std::cout << "C: ";
-        std::cin >> c;
-        std::cout << "D: ";
-        std::cin >> d;
-        std:: cout << "Текущая температура: ";
-        std::cin >> current_temperature;
-        NonlinearTemperatureCalculator* nonlinear_temperature_calculator = new NonlinearTemperatureCalculator(current_temperature, a, b, c, d);
-        temperature_viewer = new TemperatureViewer(reinterpret_cast<TemperatureCalculator*>(nonlinear_temperature_calculator));
-        break;
-    }
-    default:
-        break;
+        case 0:
+        {
+            std::cout << "A: ";
+            std::cin >> a;
+            std::cout << "B: ";
+            std::cin >> b;
+            std:: cout << "Текущая температура: ";
+            std::cin >> current_temperature;
+            TemperatureCalculator* linear_temperature_calculator = new LinearTemperatureCalculator(current_temperature, a, b);
+            temperature_viewer = new TemperatureViewer(linear_temperature_calculator);
+            break;
+        }
+        case 1:
+        {
+            std::cout << "A: ";
+            std::cin >> a;
+            std::cout << "B: ";
+            std::cin >> b;
+            std::cout << "C: ";
+            std::cin >> c;
+            std::cout << "D: ";
+            std::cin >> d;
+            std:: cout << "Текущая температура: ";
+            std::cin >> current_temperature;
+            TemperatureCalculator* nonlinear_temperature_calculator = new NonlinearTemperatureCalculator(current_temperature, a, b, c, d);
+            temperature_viewer = new TemperatureViewer(nonlinear_temperature_calculator);
+            break;
+        }
+        default:
+            break;
     }
 
     double input_warm;
     int iterations_number;
     std::cout << "Входное тепло: ";
     std::cin >> input_warm;
-    std::cout << "Количество иттераций: ";
+    std::cout << "Количество итераций: ";
     std::cin >> iterations_number;
     temperature_viewer->GetTemperaturesToConsole(iterations_number, input_warm);
     delete temperature_viewer;
