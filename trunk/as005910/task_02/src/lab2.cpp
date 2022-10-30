52afae6e8a39d40b98d563c35b3691e083d33455
Aliaksandr Dubiaha <fakehomer@mail.ru>
2022-10-30 13:01:12 +0300

    doxygen comments

--- trunk/as005910/task_02/src/lab2.cpp
+++ trunk/as005910/task_02/src/lab2.cpp
@@ -2,48 +2,76 @@
 #include <math.h>
 #include <iomanip>
 #include <fstream>
-
+/**
+* \mainpage
+* \brief ПИД-регулятор
+* \author Дубяга Александр
+*/
 using namespace std;
-
+/**
+* \class model
+* \brief Абстрактный класс, который будет использоваться для построения линейных и нелинейных моделей
+*/
 class model
 {
 public:
+    /**
+    * \details абстрактная функция для переопределения в дочерних классах
+    */
     virtual float equation(float y_t, float u_t) = 0;
 };
-
+/**
+* \class LinearMod
+* \brief Класс, представляющий линейную модель контролируемого объекта
+* \details Дочерний класс, который расширяет класс model
+*/
 class LinearMod : public model
 {
 private:
-    float a, b, y_t1;
+    float a, b, y_t1;///< a, b - просто коэффициенты, y_t1 - температура
 public:
     LinearMod(float a, float b)
     {
+    /**
+    * \details конструктор для LinearMod
+    */
         this->a = a;
         this->b = b;
     }
-
+    /**
+    * \details считаем температуру по линейной модели
+    */
     float equation(float y_t, float u_t)
     {
         y_t1 = a * y_t + b * u_t;
         return y_t1;
     }
 };
-
+/**
+* \class NonLinearMod
+* \brief Класс, представляющий нелинейную модель контролируемого объекта
+* \details Дочерний класс, который расширяет класс model
+*/
 class NonLinearMod : public model
 {
 private:
-    float a, c, d, b;
-    float y_t0 = 0, y_t1;
-    float u_t0 = 0;
+    float a, c, d, b;///< a, b, c, d - просто коэффициенты
+    float y_t0 = 0, y_t1;///< y_t0 - предыдущее(начальное) значение температуры, y_t - текущее значение температуры
+    float u_t0 = 0;///< u_t0 - переменная для предыдущего значения тепла
 public:
     NonLinearMod(float a, float b, float c, float d)
     {
+    /**
+    * \details конструктор для NonLinearMod
+    */
         this->a = a;
         this->b = b;
         this->c = c;
         this->d = d;
     }
-
+    /**
+    * \details считаем температуру по нелинейной модели
+    */
     float equation(float y_t, float u_t)
     {
         y_t1 = a * y_t - b * pow(y_t0, 2) + c * u_t + d * sin(u_t0);
@@ -53,20 +81,28 @@ public:
         return y_t1;
     }
 };
-
+/**
+* \class regulator
+* \brief Класс для реализации регулятора
+*/
 class regulator
 {
 private:
     float T, T0, TD, K, y_t1 = 0;
 public:
     regulator(float T, float T0, float TD, float K) {
+    /**
+    * \details конструктор для regulator
+    */
         this->T = T;
         this->T0 = T0;
         this->TD = TD;
         this->K = K;
     }
-
-    float warm(float e, float em1, float em2) {
+    /**
+    * \param q0, q1, q2 значения текущей, прошлой и позапрошлой ошибок
+    */
+    float temperature(float e, float em1, float em2) {
         float q0 = K * (1 + TD / T0);
         float q1 = -K * (1 + 2 * TD / T0 - T0 / T);
         float q2 = K * TD / T0;
@@ -74,6 +110,13 @@ public:
         return y_t1;
     }
 };
+/**
+* \brief Функция, которая моделирует ПИД-регулятор
+* \param w желаемое значение
+* \param *reg указатель на экземпляр regulator
+* \param *md указатель на экземпляр model
+* \param y0 начальное значение y
+*/
 void PIDregulator(float w, float y0, regulator* reg, model* md) {
     float e = 0, em1 = 0, em2 = 0, u = 0, y = y0;
     ofstream fout;
@@ -81,7 +124,7 @@ void PIDregulator(float w, float y0, regulator* reg, model* md) {
     if (fout.is_open()) {
         for (int i = 0; i < 100; i++) {
             e = w - y;
-            u = reg->warm(e, em1, em2);
+            u = reg->temperature(e, em1, em2);
             y = md->equation(y0, u);
             fout << "E=" << e << " Y=" << y << " U=" << u << endl;
             em2 = em1;
@@ -90,7 +133,9 @@ void PIDregulator(float w, float y0, regulator* reg, model* md) {
     }
     fout.close();
 }
-
+/**
+* \brief Функция main, создаём экземпляры всех классов и вызываем функцию PIDregulator
+*/
 int main()
 {
     setlocale(0, "");
