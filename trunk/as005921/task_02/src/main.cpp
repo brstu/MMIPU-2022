0417ccff5c72af1dd8c29dc2ce8904b7d59291c2
Манюк Ксения Владимировна АС-59 <93492761+ksenia232123@users.noreply.github.com>
2022-12-13 22:30:01 +0300

    Update main.cpp

--- trunk/as005921/task_02/src/main.cpp
+++ trunk/as005921/task_02/src/main.cpp
@@ -22,14 +22,14 @@ class PID_contr{
 private:
     double u = 0;
     double h = 0;
-    const double  Td = 50, T =11, T0 = 11, k = 0.1;
+    const double  Td = 50, T =11, Tk = 11, k = 0.1;
 public:
     PID_contr() {}
     double controller(double e, double e1, double e2){
-        double q0 = k*(1+(Td/T0));
-        double h = T0/T;
-        double q1 = -1*k*(1+(2*(Td/T0))-h);
-        double q2 = k * (Td/T0);
+        double q0 = k*(1+(Td/Tk));
+        double h = Tk/T;
+        double q1 = -1*k*(1+(2*(Td/Tk))-h);
+        double q2 = k * (Td/Tk);
         u += q0*e + q1*e1 + q2*e2;
         return u;
     }
